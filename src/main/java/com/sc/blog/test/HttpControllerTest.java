a2b6a6f41a00d28d6f67e4876ca7ff972cb6f2d3
dasom037 <dasom037@gmail.com>
2022-04-20 20:21:36 +0900

    test postman response

--- src/main/java/com/sc/blog/test/HttpControllerTest.java
+++ src/main/java/com/sc/blog/test/HttpControllerTest.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(html) 
@@ -20,19 +20,23 @@ public class HttpControllerTest {
 //    public String getTest(@RequestParam int id, @RequestParam String username) {
     public String getTest(Member m) {
 //        return "get 요청 : " + id + ", " + username;
-        return "get 요청 : " + m.getId() + ", " + m.getUsername();
+        return "get 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
     
     // http://localhost:8080/http/post
-    @PostMapping("/http/post")
-    public String postTest() {
-        return "post 요청";
+    @PostMapping("/http/post") // text/plain
+//    public String postTest(Member m) {
+//    public String postTest(@RequestBody String text) {
+    public String postTest(@RequestBody Member m) { // MessageConverter (스프링부트)
+//        return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+        return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+//        return "post 요청 : " + text;
     }
     
     // http://localhost:8080/http/put
     @PutMapping("/http/put")
-    public String putTest() {
-        return "put 요청";
+    public String putTest(@RequestBody Member m) {
+        return "put 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
     
     // http://localhost:8080/http/delete
