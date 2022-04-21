3f0fb7c7cf80fe19f793507821bcd35e9b1e28d8
dasom037 <dasom037@gmail.com>
2022-04-21 14:07:05 +0900

    create jsp form(login, join)

---
+++ src/main/java/com/sc/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.sc.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public String handleArgumentException(Exception e) {
+        return "<h1>" + e.getMessage() + "</h1>";
+    }
+}
